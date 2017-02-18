b802889b392e12b98ec5179cd82821dfdbab3ccc
Asmyshlyaev177 <asmyshlyaev177@gmail.com>
2017-02-18 23:16:31 +0300

    start make menu

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -26,7 +26,18 @@ $(window).load(function(){
 	hideBanner();
 	setTimeout( wsConnect(), 0 );
 	playPause();
+	userMenu();
 });
+
+function userMenu() {
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+}
 
 function hideBanner() {
   froalaBanner = $('a[href="https://froala.com/wysiwyg-editor"');
